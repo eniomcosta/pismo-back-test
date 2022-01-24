916d5e261dac07cc913d5d5b3374734c5353f949
Enio Costa <enio.m.costa@gmail.com>
2022-01-23 21:33:11 -0300

    Added Swagger

--- src/main/java/com/eniomcosta/pismobacktest/controllers/AccountController.java
+++ src/main/java/com/eniomcosta/pismobacktest/controllers/AccountController.java
@@ -4,6 +4,9 @@ import com.eniomcosta.pismobacktest.converters.AccountConverter;
 import com.eniomcosta.pismobacktest.dtos.AccountDTO;
 import com.eniomcosta.pismobacktest.entities.Account;
 import com.eniomcosta.pismobacktest.services.interfaces.AccountService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +21,11 @@ public class AccountController {
 
     private final AccountService accountService;
 
+    @ApiOperation(value = "Create account")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Return created account"),
+            @ApiResponse(code = 403, message = "Specified document number already exists"),
+    })
     @PostMapping
     public ResponseEntity<AccountDTO> create(@Valid @RequestBody AccountDTO newAccountDTO) {
         Account account = accountService.create(newAccountDTO);
@@ -25,6 +33,11 @@ public class AccountController {
         return ResponseEntity.status(HttpStatus.CREATED).body(accountDTO);
     }
 
+    @ApiOperation(value = "Find account by id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Account was found"),
+            @ApiResponse(code = 403, message = "No account was found"),
+    })
     @GetMapping(value = "/{accountId}")
     public ResponseEntity<AccountDTO> findById(@PathVariable Long accountId) {
         AccountDTO accountDTO = AccountConverter.toDto(accountService.findById(accountId));
--- src/main/java/com/eniomcosta/pismobacktest/controllers/TransactionController.java
+++ src/main/java/com/eniomcosta/pismobacktest/controllers/TransactionController.java
@@ -4,6 +4,9 @@ import com.eniomcosta.pismobacktest.converters.TransactionConverter;
 import com.eniomcosta.pismobacktest.dtos.TransactionDTO;
 import com.eniomcosta.pismobacktest.entities.Transaction;
 import com.eniomcosta.pismobacktest.services.interfaces.TransactionService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +24,11 @@ public class TransactionController {
 
     private final TransactionService transactionService;
 
+    @ApiOperation(value = "Create a transaction")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Transaction created"),
+            @ApiResponse(code = 403, message = "Validation error"),
+    })
     @PostMapping
     public ResponseEntity<TransactionDTO> create(@Valid @RequestBody TransactionDTO newTransactionDTO) {
         Transaction transaction = transactionService.create(newTransactionDTO);
