3b971c9c4e9277cf02d3881e0e56960c2f7bb5b8
Enio Costa <enio.m.costa@gmail.com>
2022-01-23 21:55:44 -0300

    Fix unused import

--- src/main/java/com/eniomcosta/pismobacktest/exceptions/RestExceptionHandler.java
+++ src/main/java/com/eniomcosta/pismobacktest/exceptions/RestExceptionHandler.java
@@ -19,7 +19,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
